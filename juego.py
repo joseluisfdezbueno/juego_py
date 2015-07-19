dc919326a2b7af4d7ecb81198dcf131385ccea4b
joseluisfdezbueno <jota_ele256@hotmail.com>
2015-07-19 18:57:28 +0200

    agregados comentarios de la estructura

--- juego.py
+++ juego.py
@@ -10,58 +10,54 @@ from pygame.locals import *
 def main():
 
     pygame.init()
-    
+
     # coords pala
     coordX_pala = 300
     coordY_pala = 600
-
+	
+	# objeto reloj
     Reloj= pygame.time.Clock()
 
+	# establecemos la ventana
     Ventana = pygame.display.set_mode((867, 650))
     pygame.display.set_caption("Juego Python")
 
 	# Cargamos las imagenes
     marcador = pygame.image.load("marcador.png")
-    Fondo = pygame.image.load("fondo7.png")
-
-    Imagen = pygame.image.load("monigotillo.png")
-    transparente = Imagen.get_at((0, 0))
-    Imagen.set_colorkey(transparente)
-    
+    fondo = pygame.image.load("fondo7.png")
     imagen_pala = pygame.image.load("pala.png")
     transparente = imagen_pala.get_at((0, 0))
     imagen_pala.set_colorkey(transparente)
     
     # Ruidito es un objeto Sound creado a partir del archivo *.wav
-    Ruidito = pygame.mixer.Sound("ouch.wav")
+    # Ruidito = pygame.mixer.Sound("ouch.wav")
 
-    # Musica es otro objeto Sound creado a partir del archivo *.wav
+    # cargamos la música de fondo
     Musica = pygame.mixer.Sound("guitarra.wav")
 
-    coordenadas_monigotillo= (300, 200)
+	# inicializamos la pala
     coordenadas_pala = (coordX_pala, coordY_pala)
-
-    MiMonigotillo = Monigotillo(coordenadas_monigotillo, Imagen)
     miPala = Pala(coordenadas_pala, imagen_pala)
 
-    # Bajamos el volumen de la música (sí, se puede hacer antes o durante la reproducción)
-    Musica.set_volume(0.5)
+    # seleccionamos el volumen
+    Musica.set_volume(0.8)
 
-    # Reproducimos nuestro sample de música en un bucle infinito (-1)
+    # reproducimos la música de fondo indefinidamente
     Musica.play(-1)
 
     while True:
 
-        MiMonigotillo.update(coordenadas_monigotillo)
+		# actualizamos las coordenadas de la pala
         miPala.update(coordenadas_pala)
-        Ventana.blit(marcador, (0, 0))
 		
-        Ventana.blit(Fondo, (14, 14))
-        Ventana.blit(MiMonigotillo.image, MiMonigotillo.rect)
+		# mostramos los sprites
+        Ventana.blit(marcador, (0, 0))		
+        Ventana.blit(fondo, (14, 14))
         Ventana.blit(miPala.image, miPala.rect)
 
         pygame.display.flip()
 
+		# tratamos los eventos generados
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 sys.exit()
@@ -76,45 +72,13 @@ def main():
 						coordX_pala = coordX_pala - 40
 
             # Si el evento es una pulsación de ratón...
-            elif evento.type == MOUSEBUTTONDOWN:
-
-                # ...actualizamos las coordenadas de monigotillo con las del puntero...
-                coordenadas_monigotillo = pygame.mouse.get_pos()
-
-                # ...y reproducimos el sonido.
-                Ruidito.play(0)
+            #elif evento.type == MOUSEBUTTONDOWN:
+               
             
+            # seleccionamos las nuevas coordenadas
             coordenadas_pala = (coordX_pala, coordY_pala)
         Reloj.tick(30)
 
-
-class Monigotillo(pygame.sprite.Sprite):
-
-    def __init__(self, coordenadas, imagen):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.ImgCompleta = imagen
-        a=0
-        self.arrayAnim=[]
-        while a < 6:
-            self.arrayAnim.append(self.ImgCompleta.subsurface((a*32,100,32,64)))
-            a= a + 1
-        self.anim= 0
-
-        self.actualizado = pygame.time.get_ticks()
-        self.image = self.arrayAnim[self.anim]
-        self.rect = self.image.get_rect()
-        self.rect.center = coordenadas
-
-
-    def update(self, nuevas_coordenadas):
-        self.rect.center = nuevas_coordenadas
-        if self.actualizado + 100 < pygame.time.get_ticks():
-            self.anim= self.anim + 1
-            if self.anim > 5:
-                self.anim= 0
-            self.image = self.arrayAnim[self.anim]
-            self.actualizado= pygame.time.get_ticks()
 
 class Pala(pygame.sprite.Sprite):
 
@@ -126,10 +90,10 @@ class Pala(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = coordenadas
 
-
     def update(self, nuevas_coordenadas):
         self.rect.center = nuevas_coordenadas
         if self.actualizado + 100 < pygame.time.get_ticks():            
             self.actualizado= pygame.time.get_ticks()
 
+# ejecutamos el juego
 main()
